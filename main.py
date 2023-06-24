ae29bd1bf39ba04ad113e97d5a9b306111ea0f2a
jeremyfukunaga <jfukunaga981@gmail.com>
2023-06-23 21:26:40 -0700

    Create main.py

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ def main():
     ##################################################
     """
 
+    # Input the number of males
+    num_males = int(input("Enter the number of males: "))
+
+    # Input the number of females
+    num_females = int(input("Enter the number of females: "))
+
+    # Calculate the total number of students
+    total_students = num_males + num_females
+
+    # Calculate the percentage of males
+    m_perc = (num_males / total_students) * 100
+
+    # Calculate the percentage of females
+    f_perc = (num_females / total_students) * 100
+
+    # Display the percentages of males and females
+    print("m_perc: {:.2f}%".format(m_perc))
+    print("f_perc: {:.2f}%".format(f_perc))
+
     """
     ########################################
     # Do not delete the return statement
